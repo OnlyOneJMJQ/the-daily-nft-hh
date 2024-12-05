a32d54c3b81986c98da4e3c578db73e40fddc625
Josh Quintal <joshquin@gmail.com>
2024-12-05 11:49:05 +0800

    Increase BB network timeout

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-    networks: {
-        buildbear: {
-            url: "https://rpc.buildbear.io/criminal-lockjaw-e9468812",
-        },
+  networks: {
+    buildbear: {
+      url: "https://rpc.buildbear.io/criminal-lockjaw-e9468812",
+      timeout: 100_000,
     },
-    solidity: "0.8.24",
+  },
+  solidity: "0.8.24",
 };
 
 export default config;
